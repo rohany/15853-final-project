d3eeaed2530548ef7270f96acd15673dd2cfddce
Rohan <rohany@RohanYadav-PC.localdomain>
2018-04-10 00:43:01 -0400

    more infra changes

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <chrono>
 
 #include "k-merge.hpp"
 #include "standard-sort.hpp"
@@ -26,7 +28,67 @@ int main(int argc, char** argv) {
     B = atoi(argv[4]);
   }
 
-  std::cout << mode << N << M << B << std::endl;
+  // Look into using mmap here?
+  int* default_in = new int[N];
+  int* default_out = new int[N];
 
-  return 0;
+  int* io_in = new int[N];
+  int* io_out = new int[N];
+
+  int* input = new int[N];
+
+  std::cout << "Input allocation complete" << std::endl;
+
+  std::generate(input, input + N, std::rand);
+
+  std::memcpy(default_in, input, N);
+  std::memcpy(io_in, input, N);
+
+  std::cout << "Input generation complete" << std::endl;
+
+  delete[] input;
+
+
+  if (mode == "buffer") {
+    std::cout << "not supported yet!" << std::endl;
+    return 0;
+  }
+  else if (mode == "merge") {
+    StandardSort stdsort;
+    KMergeSort iosort(M, B);
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    stdsort.sort(default_in, N, default_out);
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "Standard sort time elapsed = " <<
+      std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <<std::endl;
+
+    // TODO: verify correctness
+
+    delete[] default_in;
+    delete[] default_out;
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    iosort.sort(io_in, N, io_out);
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "IO efficient sort time elapsed = " <<
+      std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <<std::endl;
+
+    // TODO: verify correctness
+
+    delete[] io_in;
+    delete[] io_out;
+
+    return 0;
+  }
+  else {
+    std::cout << "Unsupported sorting algorithm" << std::endl;
+    return 1;
+  }
 }
